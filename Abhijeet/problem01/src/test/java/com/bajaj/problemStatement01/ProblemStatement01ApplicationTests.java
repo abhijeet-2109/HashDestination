211b6989f03b7b691fab33781806279c041135f4
abhijeet-2109 <abhijeetjambkar2109@gmail.com>
2024-08-28 22:04:15 +0530

    problem01

---
+++ Abhijeet/problem01/src/test/java/com/bajaj/problemStatement01/ProblemStatement01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajaj.problemStatement01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemStatement01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
